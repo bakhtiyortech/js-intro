4a7d200b222245a0e9c26a8f51fb445e6fa4829e
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-03-28 18:22:23 -0400

    Finish Interest Calculator Assignment

---
+++ assignments/assignment-3/interest-calculator.js
@@ -0,0 +1,10 @@
+// Interest Calculator
+// Declare variables principal, rate, and time, assigning them appropriate values for an interest calculation (you can decide the values). 
+//Compute the simple interest with the formula Interest = (Principal * Rate * Time) / 100 and store it in a variable interest. 
+//Log the interest to the console.
+
+var principal = 500;
+var rate = 4.99;
+var time = 7;
+var interest = (principal * rate *time) / 100;
+console.log("Your interest:", interest);
