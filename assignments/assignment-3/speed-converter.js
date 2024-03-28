ab86579c294fdae8dc7e3c220273559873ae737c
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-03-28 19:05:51 -0400

    Finish Speed Convertert Assignment

---
+++ assignments/assignment-3/speed-converter.js
@@ -0,0 +1,8 @@
+// Speed Converter
+//Declare a variable milesPerHour and assign it a value representing speed in miles per hour. 
+//Convert this speed to kilometers per hour and assign the result to a variable kilometersPerHour. 
+// The formula for conversion is km/h = mph * 1.60934. Log the kilometers per hour to the console.
+
+var milesPerHour = 60;
+var kilometersPerHour = milesPerHour * 1.60934;
+console.log(kilometersPerHour);
