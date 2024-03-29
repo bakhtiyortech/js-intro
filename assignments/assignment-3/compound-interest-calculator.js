8a79479fcf38300b9a0ca148a9f3d27e581ec560
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-03-28 20:13:17 -0400

    Finish Compound Interest Calculator Assignment

---
+++ assignments/assignment-3/compound-interest-calculator.js
@@ -0,0 +1,21 @@
+// Compound Interest Calculator
+
+// Declare variables principal, annualRate, and years, assigning them appropriate values for a compound interest calculation. 
+// Compute the compound interest with the formula A = P(1 + r/n)^(nt), where:
+
+// P is the principal amount.
+
+// r is the annual interest rate (decimal).
+
+// n is the number of times interest is compounded per year.
+
+// t is the time the money is invested for in years. 
+// Store the total amount after interest in a variable totalAmount and log it to the console. 
+// Note: You can assume n to be a fixed value like 4.
+
+var principal = 10000;
+var annualRate = 0.07;
+var years = 5;
+var n = 4;
+var totalAmount = principal * (1 + annualRate/n)^(n * years);
+console.log(totalAmount);
