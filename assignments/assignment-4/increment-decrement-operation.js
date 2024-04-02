623e9d7c7d9de1cc09685625e5dcc3bd6cdd59bb
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-01 22:05:24 -0400

    Finish Increment and Decrement Operators Assignment

---
+++ assignments/assignment-4/increment-decrement-operation.js
@@ -0,0 +1,12 @@
+// Using Increment and Decrement Operators
+
+// Apply the increment operator (++) to num1 and print its new value.
+
+// Apply the decrement operator (--) to num2 and print its new value.
+
+var num1 = 10;
+var num2 = 3;
+num1 ++;
+num2 --;
+console.log("Increment:", num1);
+console.log("Decrement:", num2);
