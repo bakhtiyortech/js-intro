24f900bf4d8b349ec9f435220a69d074f0ef920d
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-02 14:16:47 -0400

    Finish String Concatenation Assignment

---
+++ assignments/assignment-5/method-operator.js
@@ -0,0 +1,53 @@
+// Objective
+// You've been tasked with composing a famous quote using separate parts of a sentence. Each part of the sentence will be stored in a different variable. You are to assemble these parts together to reveal the final quote.
+
+// Instruction
+// Here are the sentence parts:
+
+// Variable part1: "The greatest glory in"
+
+// Variable part2: "living lies not in"
+
+// Variable part3: "never falling, but in"
+
+// Variable part4: "rising every time"
+
+// Variable part5: "we fall."
+
+// Variable author: " - Nelson Mandela"
+
+// The final quote should look like this: 
+
+// "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
+
+// Your challenge is to create this quote four separate times. Each time, use a different string concatenation method:
+
+// First, use the template literal method.
+
+// Second, use the .concat() method.
+
+// Third, use the + operator.
+
+// Fourth, use the += operator.
+
+// After you've completed all four methods, compare the results. Make sure each version of the quote matches the final quote exactly
+
+var part1 = "The greatest glory in";
+var part2 = "living lies not in";
+var part3 = "never falling, but in";
+var part4 = "rising every time";
+var part5 = "we fall.";
+var author = "- Nelson Mandela";
+
+// First, use the template literal method.
+console.log(`${part1} ${part2} ${part3} ${part4} ${part5} ${author} `);
+
+// Second, use the .concat() method.
+console.log(part1.concat(" ",part2, " ",part3," ",part4," ",part5," ",author));
+
+// Third, use the + operator.
+console.log(part1 + " " + part2 + " " + part3 + " " + part4 + " " + part5 + " " + author);
+
+// Fourth, use the += operator.
+console.log(part1 += "",part2 += "",part3 += "",part4 += "",part5 += "",author);
+
