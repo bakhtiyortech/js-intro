e9eabadc1b599ae597f4667e2c5ca32b87d21abd
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-02 13:30:49 -0400

    Finish Comprasion Operation Assignment

---
+++ assignments/assignment-4/comparison-operation.js
@@ -0,0 +1,15 @@
+// Comparison Operations
+
+// Compare if num1 is greater than num2 and print the boolean result.
+
+// Determine if num1 is less than or equal to num2 and print the boolean result.
+
+// Check whether num1 multiplied by num2 is greater than or equal to total and print the boolean outcome
+
+var num1 = 18;
+var num2 = 7;
+var total = 32;
+
+console.log(num1 > num2);
+console.log(num1 <= num2);
+console.log(num1 * num2 >= total);
