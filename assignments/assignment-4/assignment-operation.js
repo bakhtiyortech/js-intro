6f877f771c9ff6074e54204c4963d0067993b3d8
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-02 13:11:20 -0400

    Finish Assignment Operation

---
+++ assignments/assignment-4/assignment-operation.js
@@ -0,0 +1,25 @@
+// Assignment Operations
+
+// Initialize a new variable total with zero, then add num1 to num2 using the += operator on total. Print total.
+
+// Subtract num1 from total using the -= operator and print total.
+
+// Multiply total by num2 using the *= operator and print total.
+
+// Divide total by num1 using the /= operator and print the final value of total.
+
+var total = 0;
+var num1 = 30;
+var num2 = 10;
+
+total += (num1 + num2);
+console.log(total);
+
+total -= num1;
+console.log(total);
+
+total *= num2;
+console.log(total);
+
+total /= num1;
+console.log(total);
