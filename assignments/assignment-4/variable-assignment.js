b8927d9835dc5c37966dbec7e0228dd1f75f3273
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-02 13:42:54 -0400

    Finish Variable Assignment

---
+++ assignments/assignment-4/variable-assignment.js
@@ -0,0 +1,12 @@
+// Final Task: Understanding Variable Assignment
+
+// Assign the result of num1 plus num2 minus total to a new variable named result. Use the assignment operator (=) for this task.
+
+// Display the value of result.
+
+var num1 = 27;
+var num2 = 9;
+var total = 14;
+var result = num1 + num2 - total;
+
+console.log(result);
