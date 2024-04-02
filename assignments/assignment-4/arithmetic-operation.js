e0b7577bf3ba5ad1b26f486114b6ef40c2bd9f74
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-01 21:32:38 -0400

    Finish Arithmetic Operation Assignment

---
+++ assignments/assignment-4/arithmetic-operation.js
@@ -0,0 +1,18 @@
+// Arithmetic Operations Practice
+
+// Declare two variables, num1 and num2, assigning them any two positive integers of your choice.
+
+// Perform addition, subtraction, multiplication, and division on num1 and num2. Print each result.
+
+// Calculate the remainder (modulo) when num1 is divided by num2 and print the outcome.
+
+// Raise num1 to the power of num2 and output the result
+
+var num1 = 10;
+var num2 = 3;
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulo:", num1 % num2);
+console.log("Exponentiation:", num1 ** num2);
