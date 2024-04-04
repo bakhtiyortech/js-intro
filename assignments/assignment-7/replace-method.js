5b607495b950ee38bcf8596063f5c669cfc027b3
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:48:26 -0400

    Finish Replace Method Assignment

---
+++ assignments/assignment-7/replace-method.js
@@ -0,0 +1,7 @@
+// .replace() method
+
+// Create a string variable feedback with the value "This product is good!".
+// Use the replace method to change "good" to "excellent" and log the new string
+
+var str = "This product is good!";
+console.log(str.replace("good", "excellent"));
