878ab90f19d329cf74d234c37fa26433e5133314
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 14:30:59 -0400

    Finish Length Property Assignment

---
+++ assignments/assignment-7/length-property.js
@@ -0,0 +1,20 @@
+// .length property
+
+
+// Declare Two String Variables:
+
+// Create two string variables, post1 and post2, assigning them text values of your choice. 
+// These will represent two different social media posts that you will compare.
+
+// Compare Lengths and Output Results:
+
+// Write code to compare the lengths of post1 and post2.  Check if post1 is longer, shorter, or equal in length to post2 . 
+// Store a result in a variable named comparisonResult . Output the value of comparisonResult to the console.
+
+var post1 = "My name is Bakhtiyor.";
+var post2 = "I am from  Uzbekistan.";
+var comparisonResult = post1.length <= post2.length;
+console.log(comparisonResult); // true
+
+var comparisonResult = post1.length >= post2.length;
+console.log(comparisonResult); // false
