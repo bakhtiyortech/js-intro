3aa55ce5e5a5d073218adcad59b5e3147e766ec1
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:39:17 -0400

    Finish Includes Method Assignment

---
+++ assignments/assignment-7/includes-method.js
@@ -0,0 +1,7 @@
+// .includes() method
+
+// Given the string "Looking for the hidden gems in the city.", use the includes method to check if it contains the word "hidden".
+// Store the boolean result in a variable and log it
+
+var str = "Looking for the hidden gems in the city."
+console.log(str.includes("hidden")); // true
