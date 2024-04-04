0de64f2550a8c9a3ca0e54e93fad9e246c295c71
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:56:48 -0400

    Finish toUpperCase Method Assignment

---
+++ assignments/assignment-7/to-upper-case-method.js
@@ -0,0 +1,6 @@
+// .toUpperCase() method
+
+// Given a variable shout with the value "can you hear me?", convert it to uppercase using toUpperCase and log the result.
+
+var shout = "can you hear me?";
+console.log(shout.toUpperCase());
