8b33cd32384c9ec386518b0aa3e37b8fb19588d1
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:32:38 -0400

    Finish endWith Method Assignment

---
+++ assignments/assignment-7/endwith-method.js
@@ -0,0 +1,7 @@
+// .endWith method
+
+// Declare a variable announcement with the value "Today's event starts at 3 PM!".
+// Use the endsWith method to check if announcement ends with "PM!". Store the result in a variable and log it.
+
+var announcement = "Today's event starts at 3 PM!";
+console.log(announcement.endsWith("PM!")); // true
