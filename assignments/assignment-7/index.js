e901f7f9c61a12082a7689903f1e46a0a2c1f32b
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:07:42 -0400

    Finish Index Assignment

---
+++ assignments/assignment-7/index.js
@@ -0,0 +1,18 @@
+// index[]
+
+// Accessing the Beginning Characters:
+
+// Declare a string variable greeting with the value "Hello, world!".
+// Log the first, second, and third characters of the string to the console individually.
+
+// Accessing the Last Characters:
+
+// Explain how to access the last character of the greeting string.
+// Log the last character and the one before it (the second-to-last character) to the console.
+
+var greeting = "Hello, world!";
+console.log(greeting[0]);
+console.log(greeting[1]);
+console.log(greeting[2]);
+console.log(greeting[greeting.length - 1]);
+console.log(greeting[greeting.length - 2]);
