15284686a4a9cd31b1ec1ca5e58ccb88b9a0b285
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:43:50 -0400

    Finish indexOf Method Assignment

---
+++ assignments/assignment-7/indexof-method.js
@@ -0,0 +1,6 @@
+// .indexOf method
+// With the string "Repeat after me: JavaScript is fun!", use the indexOf method to find the position of the substring "JavaScript".
+// Log the position to the console
+
+var str = "Repeat after me: JavaScript is fun!";
+console.log(str.indexOf("JavaScript")); // 17
