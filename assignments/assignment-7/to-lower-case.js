b7b723d2f2f55f18cea62c7dadc5239050fbec45
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:57:32 -0400

    Finish toLowerCase Method Assignment

---
+++ assignments/assignment-7/to-lower-case.js
@@ -0,0 +1,6 @@
+// toLowerCase() method
+
+// Conversely, take the string "PLEASE LOWER YOUR VOICE" and convert it to lowercase using toLowerCase. Log the outcome 
+
+var str = "PLEASE LOWER YOUR VOICE";
+console.log(str.toLowerCase());
