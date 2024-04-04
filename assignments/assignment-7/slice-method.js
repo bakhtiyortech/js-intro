36c21e7fa5e2370b5a66748a4c09553895e05361
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 16:19:56 -0400

    Finish Slice Method Assignment

---
+++ assignments/assignment-7/slice-method.js
@@ -0,0 +1,6 @@
+// .slice() method
+// Given "Hello, world!", use the slice method to extract "Hello" and "world!" in separate variables and log them.
+
+var str = "Hello, world!";
+console.log(str.slice(0, 5));
+console.log(str.slice(7));
