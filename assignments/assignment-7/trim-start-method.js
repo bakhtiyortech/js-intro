6b2ba35f6e0edba8848ee3c78cbc763aea515e93
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 16:06:08 -0400

    Finish trimStart Method Assignment

---
+++ assignments/assignment-7/trim-start-method.js
@@ -0,0 +1,6 @@
+// trimStart() method
+
+// For the string " JavaScript is awesome! ", apply trim, trimStart, and trimEnd methods in separate log statements to see their effects.
+
+var str = " JavaScript is awesome! ";
+console.log(str.trimStart());
