9c3e14db1d163e7adb7b15a34dc515d8026a1963
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 16:09:39 -0400

    Finish trimEnd Method Assignment

---
+++ assignments/assignment-7/trim-end-method.js
@@ -0,0 +1,6 @@
+// trimEnd() method 
+
+// For the string " JavaScript is awesome! ", apply trim, trimStart, and trimEnd methods in separate log statements to see their effects.
+
+var str = " JavaScript is awesome! ";
+console.log(str.trimEnd());
