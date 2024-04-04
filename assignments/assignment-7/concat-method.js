8da77de97a42c431c54817c717851d85bfb9505a
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 15:31:56 -0400

    Finish Concat Method Assignment

---
+++ assignments/assignment-7/concat-method.js
@@ -0,0 +1,11 @@
+// .concat method
+
+// Create two variables, greeting and name, with string values "Hello, " and "Alice" respectively.
+// Use the concat method to combine them into a third variable, welcomeMessage.
+// Log welcomeMessage to the console.
+
+var greeting = "Hello";
+var name = "Alice";
+
+var welcomeMessage = greeting.concat(" " + name);
+console.log(welcomeMessage);
