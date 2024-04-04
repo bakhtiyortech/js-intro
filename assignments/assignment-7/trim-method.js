01b851f658d963e58690bf86f668767070ce689a
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-04 16:02:10 -0400

    Finish Trim Method Assignment

---
+++ assignments/assignment-7/trim-method.js
@@ -0,0 +1,6 @@
+// // trim() method
+
+// For the string " JavaScript is awesome! ", apply trim method in separate log statements to see their effects.
+
+var str = " JavaScript is awesome! ";
+console.log(str.trim());
