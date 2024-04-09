a2be366849ca2e8eb8f4d7e9be01cc4a3737f1d6
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-09 16:53:55 -0400

    Modify Codes

--- assessment/assessment-3/challenge-4.js
+++ assessment/assessment-3/challenge-4.js
@@ -7,5 +7,8 @@
 // This means that whether your name starts with a lowercase or uppercase letter, it should still be removed from the alphabet string. Print the result. 
 
  var alphabet = "abcdefghijklmnopqrstuvwxyz";
- var alphabetWithoutMyNameFirstCharacter = alphabet[0].concat(alphabet.slice(2));
+ var nameFirstLetter = "b";
+ var nameFirstLetterIdx = alphabet.indexOf(nameFirstLetter);
+
+ var alphabetWithoutMyNameFirstCharacter = alphabet.slice(0, nameFirstLetterIdx) + alphabet.slice(nameFirstLetterIdx + 1);
  console.log(alphabetWithoutMyNameFirstCharacter);
