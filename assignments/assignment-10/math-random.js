39feb4386dfb3b12bbdb4bc78ef03cdc911a7082
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-11 07:10:41 -0400

    Finish Math Random Method Assignment

---
+++ assignments/assignment-10/math-random.js
@@ -0,0 +1,171 @@
+/* ----- Math.random() method ----- */
+
+// Between 1 and 10
+
+// 1
+var numberMax = 10;
+var randomNumber = (Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 2
+var numberMax = 10;
+var randomNumber = Math.floor(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 3
+var numberMax = 10;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// Between 1 and 100
+
+// 1
+var numberMax = 100;
+var randomNumber = (Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 2
+var numberMax = 100;
+var randomNumber = Math.floor(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 3
+var numberMax = 100;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// Between 50 and 100
+
+// 1
+var numberMax = 100 - 49;
+var randomNumber = (Math.random() * numberMax) + 50;
+console.log(randomNumber);
+
+// 2
+var numberMax = 100 - 49;
+var randomNumber = Math.floor(Math.random() * numberMax) + 50;
+console.log(randomNumber);
+
+// 3
+var numberMax = 100 - 49;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 50;
+console.log(randomNumber);
+
+// Between -10 and 10
+
+// 1
+var numberMax = 10;
+var randomNumber = (Math.random() * (numberMax + 11)) - 10;
+console.log(randomNumber);
+
+// 2
+var numberMax = 10;
+var randomNumber = Math.floor(Math.random() * (numberMax + 11)) - 10;
+console.log(randomNumber);
+
+// 3
+var numberMax = 10;
+var randomNumber = Math.ceil(Math.random() * (numberMax + 11)) - 10;
+console.log(randomNumber);
+
+// Between -100 and 100
+
+// 1
+var numberMax = 10;
+var randomNumber = (Math.random() * (numberMax + 101)) - 100;
+console.log(randomNumber);
+
+// 2
+var numberMax = 10;
+var randomNumber = Math.floor(Math.random() * (numberMax + 101)) - 100;
+console.log(randomNumber);
+
+// 3
+var numberMax = 10;
+var randomNumber = Math.ceil(Math.random() * (numberMax + 101)) - 100;
+console.log(randomNumber);
+
+// Between 1000 and 2000
+
+// 1
+var numberMax = 2000 - 999;
+var randomNumber = (Math.random() * numberMax) + 1000;
+console.log(randomNumber);
+
+// 2
+var numberMax = 2000 - 999;
+var randomNumber = Math.floor(Math.random() * numberMax) + 1000;
+console.log(randomNumber);
+
+// 3
+var numberMax = 2000 - 999;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 1000;
+console.log(randomNumber);
+
+// Between -1000 and 0
+
+// 1
+var numberMax = 0;
+var randomNumber = (Math.random() * (numberMax + 1000)) - 1000;
+console.log(randomNumber);
+
+// 2
+var numberMax = 0;
+var randomNumber = Math.floor(Math.random() * (numberMax + 1000)) - 1000;
+console.log(randomNumber);
+
+// 3
+var numberMax = 0;
+var randomNumber = Math.ceil(Math.random() * (numberMax + 1000)) - 1000;
+console.log(randomNumber);
+
+// Between 1 and 1000
+
+// 1
+var numberMax = 1000;
+var randomNumber = (Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 2
+var numberMax = 1000;
+var randomNumber = Math.floor(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// 3
+var numberMax = 1000;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+console.log(randomNumber);
+
+// Between -5000 and 500
+
+// 1
+var numberMax = 500;
+var randomNumber = (Math.random() * (numberMax + 501)) - 500;
+console.log(randomNumber);
+
+// 2
+var numberMax = 500;
+var randomNumber = Math.floor(Math.random() * (numberMax + 501)) - 1000;
+console.log(randomNumber);
+
+// 3
+var numberMax = 500;
+var randomNumber = Math.ceil(Math.random() * (numberMax + 501)) - 1000;
+console.log(randomNumber);
+
+// Between 10000 and 20000
+
+// 1
+var numberMax = 20000 - 9999;
+var randomNumber = (Math.random() * numberMax) + 10000;
+console.log(randomNumber);
+
+// 2
+var numberMax = 20000 - 9999;
+var randomNumber = Math.floor(Math.random() * numberMax) + 10000;
+console.log(randomNumber);
+
+// 3
+var numberMax = 20000 - 9999;
+var randomNumber = Math.ceil(Math.random() * numberMax) + 10000;
+console.log(randomNumber);
