93e8fdd6d48402dfae525c0e0eb452de4bf7f3e7
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-12 19:55:37 -0400

    Modify Assignment-10

--- assignments/assignment-10/math-random.js
+++ assignments/assignment-10/math-random.js
@@ -2,52 +2,37 @@
 
 // Between 1 and 10
 
-// 1
-var numberMax = 10;
-var randomNumber = (Math.random() * numberMax) + 1;
-console.log(randomNumber);
-
-// 2
+//  1
 var numberMax = 10;
 var randomNumber = Math.floor(Math.random() * numberMax) + 1;
 console.log(randomNumber);
 
-// 3
+// 2
 var numberMax = 10;
-var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+var randomNumber = Math.ceil(Math.random() * numberMax);
 console.log(randomNumber);
 
 // Between 1 and 100
 
 // 1
 var numberMax = 100;
-var randomNumber = (Math.random() * numberMax) + 1;
+var randomNumber = Math.floor(Math.random() * numberMax) + 1;
 console.log(randomNumber);
 
 // 2
 var numberMax = 100;
-var randomNumber = Math.floor(Math.random() * numberMax) + 1;
-console.log(randomNumber);
-
-// 3
-var numberMax = 100;
-var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+var randomNumber = Math.ceil(Math.random() * numberMax);
 console.log(randomNumber);
 
 // Between 50 and 100
 
 // 1
-var numberMax = 100 - 49;
-var randomNumber = (Math.random() * numberMax) + 50;
-console.log(randomNumber);
-
-// 2
-var numberMax = 100 - 49;
+var numberMax = 100 - 50;
 var randomNumber = Math.floor(Math.random() * numberMax) + 50;
 console.log(randomNumber);
 
-// 3
-var numberMax = 100 - 49;
+// 2
+var numberMax = 100 - 50;
 var randomNumber = Math.ceil(Math.random() * numberMax) + 50;
 console.log(randomNumber);
 
@@ -55,15 +40,10 @@ console.log(randomNumber);
 
 // 1
 var numberMax = 10;
-var randomNumber = (Math.random() * (numberMax + 11)) - 10;
-console.log(randomNumber);
-
-// 2
-var numberMax = 10;
 var randomNumber = Math.floor(Math.random() * (numberMax + 11)) - 10;
 console.log(randomNumber);
 
-// 3
+// 2
 var numberMax = 10;
 var randomNumber = Math.ceil(Math.random() * (numberMax + 11)) - 10;
 console.log(randomNumber);
@@ -72,15 +52,10 @@ console.log(randomNumber);
 
 // 1
 var numberMax = 10;
-var randomNumber = (Math.random() * (numberMax + 101)) - 100;
-console.log(randomNumber);
-
-// 2
-var numberMax = 10;
 var randomNumber = Math.floor(Math.random() * (numberMax + 101)) - 100;
 console.log(randomNumber);
 
-// 3
+// 2
 var numberMax = 10;
 var randomNumber = Math.ceil(Math.random() * (numberMax + 101)) - 100;
 console.log(randomNumber);
@@ -88,84 +63,59 @@ console.log(randomNumber);
 // Between 1000 and 2000
 
 // 1
-var numberMax = 2000 - 999;
-var randomNumber = (Math.random() * numberMax) + 1000;
+var numberMax = 2000 - 1000;
+var randomNumber = Math.floor(Math.random() * (numberMax + 1)) + 1000;
 console.log(randomNumber);
 
 // 2
-var numberMax = 2000 - 999;
-var randomNumber = Math.floor(Math.random() * numberMax) + 1000;
-console.log(randomNumber);
-
-// 3
-var numberMax = 2000 - 999;
-var randomNumber = Math.ceil(Math.random() * numberMax) + 1000;
+var numberMax = 2000 - 1000;
+var randomNumber = Math.ceil(Math.random() * (numberMax) + 1000;
 console.log(randomNumber);
 
 // Between -1000 and 0
 
 // 1
-var numberMax = 0;
-var randomNumber = (Math.random() * (numberMax + 1000)) - 1000;
+var numberMax = 0 + 1000;
+var randomNumber = Math.floor(Math.random() * (numberMax + 1)) - 1000;
 console.log(randomNumber);
 
 // 2
 var numberMax = 0;
-var randomNumber = Math.floor(Math.random() * (numberMax + 1000)) - 1000;
-console.log(randomNumber);
-
-// 3
-var numberMax = 0;
-var randomNumber = Math.ceil(Math.random() * (numberMax + 1000)) - 1000;
+var randomNumber = Math.ceil(Math.random() * (numberMax)) - 1000;
 console.log(randomNumber);
 
 // Between 1 and 1000
 
 // 1
 var numberMax = 1000;
-var randomNumber = (Math.random() * numberMax) + 1;
-console.log(randomNumber);
-
-// 2
-var numberMax = 1000;
 var randomNumber = Math.floor(Math.random() * numberMax) + 1;
 console.log(randomNumber);
 
-// 3
+// 2
 var numberMax = 1000;
-var randomNumber = Math.ceil(Math.random() * numberMax) + 1;
+var randomNumber = Math.ceil(Math.random() * numberMax);
 console.log(randomNumber);
 
-// Between -5000 and 500
+// Between -500 and 500
 
 // 1
-var numberMax = 500;
-var randomNumber = (Math.random() * (numberMax + 501)) - 500;
+var numberMax = 500 - 500;
+var randomNumber = Math.floor(Math.random() * (numberMax + 1)) - 1000;
 console.log(randomNumber);
 
 // 2
-var numberMax = 500;
-var randomNumber = Math.floor(Math.random() * (numberMax + 501)) - 1000;
-console.log(randomNumber);
-
-// 3
-var numberMax = 500;
-var randomNumber = Math.ceil(Math.random() * (numberMax + 501)) - 1000;
+var numberMax = 500 - 500;
+var randomNumber = Math.ceil(Math.random() * (numberMax + 1)) - 1000;
 console.log(randomNumber);
 
 // Between 10000 and 20000
 
 // 1
-var numberMax = 20000 - 9999;
-var randomNumber = (Math.random() * numberMax) + 10000;
-console.log(randomNumber);
-
-// 2
-var numberMax = 20000 - 9999;
+var numberMax = 20000 - 10000;
 var randomNumber = Math.floor(Math.random() * numberMax) + 10000;
 console.log(randomNumber);
 
-// 3
-var numberMax = 20000 - 9999;
+// 2
+var numberMax = 20000 - 10000;
 var randomNumber = Math.ceil(Math.random() * numberMax) + 10000;
 console.log(randomNumber);
