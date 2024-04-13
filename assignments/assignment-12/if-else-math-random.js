59896e10f8d04b4564794c21ae28700cc4185b4e
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-12 22:22:48 -0400

    Finish If Else Statement with Math Random Assignment

---
+++ assignments/assignment-12/if-else-math-random.js
@@ -0,0 +1,28 @@
+/* ----- 1 ----- */
+
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var userName = "Jennifer";
+
+if (randomNumber >= 1 && randomNumber <= 3){
+    console.log(userName.slice(0, 3));
+} else if (randomNumber >= 4 && randomNumber <= 7){
+    console.log(userName.slice(0, 6));
+} else  {
+    console.log(userName);
+}
+
+
+/* ----- 2 ----- */
+
+var randomNumber2 = Math.floor(Math.random() * 4) + 1;
+var name = "Abbos";
+
+if (randomNumber2 === 1){
+    console.log(`Dear ${name}, wishing you a day filled with happiness and a year filled with joy.`);
+} else if (randomNumber2 === 2){
+    console.log(`Dear ${name}, sending you smiles for every moment of your special day.`);
+} else if ( randomNumber2 === 3){
+    console.log(`Dear ${name}, have a wonderful time and a very happy day!`);
+} else {
+    console.log(`Dear ${name}, hope your special day brings you all that your heart desires!`);
+}
