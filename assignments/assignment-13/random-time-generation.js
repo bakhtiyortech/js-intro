6a63bfc0a492572049c4fbc5aac458cd60ae0f8a
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-13 18:48:12 -0400

    Add time variable and Change If statement

--- assignments/assignment-13/random-time-generation.js
+++ assignments/assignment-13/random-time-generation.js
@@ -1,21 +1,24 @@
 var randomSeconds = Math.floor(Math.random() * 86400 + 1);
 
 var hours = Math.floor(randomSeconds / 3600);
-console.log(hours);
+// console.log(hours);
 var hoursInSeconds = hours * 3600;
 var minutes = Math.floor((randomSeconds - hoursInSeconds) / 60);
 var minutesInSeconds = minutes * 60;
 var seconds = randomSeconds - hoursInSeconds - minutesInSeconds;
 
+var time = `${hours}:${minutes}:${seconds}`;
+// console.log(time);
+
 if (hours >= 6 && hours < 12){
-    console.log("Good morning!");
+    console.log(time + " " + "Good morning!");
 } else if (hours >= 12 && hours < 17){
-    console.log("Good afternoon!");
+    console.log(time + " " + "Good afternoon!");
 } else {
-    console.log("Good evening!");
+    console.log(time + " " + "Good evening!");
 }
 if (hours === 24 && minutes === 0 && seconds === 0){
-    console.log("The new day starting!");
+    console.log(time + " " + "The new day starting!");
 }
 
 
