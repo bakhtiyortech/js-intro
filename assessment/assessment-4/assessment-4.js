e6a19479da9d95b46dcfad5a0439f16692f9de7f
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-17 07:06:16 -0400

    Complete Assessment-4

--- assessment/assessment-4/assessment-4.js
+++ assessment/assessment-4/assessment-4.js
@@ -17,7 +17,7 @@ console.log(convertToUpperCase);
 var randomNumber = Math.floor((Math.random() * 1000) + 1);
 var oddNumber = randomNumber % 2 === 0;
 console.log(oddNumber);
-var evenNumber = randomNumber % 2 === 1;
+var evenNumber = randomNumber % 2 !== 0;
 console.log(evenNumber);
 
 /* ----- challenge-3 ----- */
@@ -28,3 +28,22 @@ var divideNumbers = numerator / denominator;
 console.log(divideNumbers);
 var roundResult = Math.round(divideNumbers);
 console.log(roundResult);
+
+/* ----- challenge-4 ----- */
+
+var testNumber = Math.floor(Math.random() * 2476) + 25;
+var testNumberDivisible3Not5 = testNumber % 3 === 0 && testNumber % 5 !== 0;
+console.log(testNumberDivisible3Not5);
+
+/* ----- challenge-5 ----- */
+
+var divTest = Math.floor(Math.random() * 101) + 100;
+var divTestDivisible5Not3 = testNumber % 3 === 0 && testNumber % 5 !== 0;
+console.log(testNumberDivisible3Not5);
+
+/* ----- challenge-6 ----- */
+
+var creativeString = "Exploring the vast universe of code can lead to unexpected discoveries."
+var creativeStringRandomIdx = Math.floor(Math.random() * creativeString.length);
+var textAfterRandom = creativeString.slice(creativeStringRandomIdx);
+console.log(textAfterRandom);
