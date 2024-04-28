5481e4c924a3ced97c56ef3594438e7de95aa638
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-28 16:48:12 -0400

    Add Challenge-2

--- assignments/assignment-18/loops.js
+++ assignments/assignment-18/loops.js
@@ -8,3 +8,20 @@ function countDivisibleByThreeOrFive(n) {
         }
     }
 }
+
+/* ----- Challenge-2 ----- */
+
+function sumUpToBoth(num1, num2) {
+    var sum1 = 0;
+    for (var i = 1; i <= num1; i++) {
+        return sum1 += i;
+    }
+
+    var sum2 = 0;
+    for (var i = 1; i <= num2; i++) {
+        return sum2 += i;
+    }
+
+    var totalSum = sum1 + sum2;
+    return totalSum;
+}
