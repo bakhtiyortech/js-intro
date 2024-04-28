b4bce2d66568e8d36034e42a11cc3a00af9ee7f5
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-27 22:06:38 -0400

    Finish Challenge-2 on the Assignment-17

--- assignments/assignment-17/loop.js
+++ assignments/assignment-17/loop.js
@@ -3,7 +3,16 @@
 function sumExclusiveRange(minNum, maxNum) {
     var sum = 0;
     for (var i = minNum; i < maxNum; i++) {
-        console.log(sum += i);
+        return sum += i;
     }
-    if (minNum > maxNum) console.log("Not valid range");
+    if (minNum > maxNum) return "Not valid range";
+}
+
+/* ----- Challenge-2 ----- */
+
+function sumEvenNumbers(num) {
+    var sum = 0;
+    for (var i = 0; i < num; i++) {
+        if (i % 2 === 0) return sum += i;
+    }
 }
