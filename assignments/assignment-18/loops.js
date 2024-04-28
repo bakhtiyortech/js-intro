fb009825ef48f04627a2f34116614eda9251c565
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-04-28 17:42:12 -0400

    Fix Challenge-5

--- assignments/assignment-18/loops.js
+++ assignments/assignment-18/loops.js
@@ -55,7 +55,9 @@ function countDigitsSumDivisibleByThree(num1, num2) {
     var sum = 0;
     for (var i = num1; i <= num2; i++) {
         if (i % 3 === 0) {
-            return sum++;
+            return sum += 1;
         }
     }
 }
+
+countDigitsSumDivisibleByThree(7, 15);
