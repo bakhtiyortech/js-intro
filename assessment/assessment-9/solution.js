5b74f40b36b602068106f338053714c275797255
Bakhtiyor Khodjaev <bakhtiyor.tech@gmail.com>
2024-05-06 12:29:59 -0400

    Finish Assessment-9

--- assessment/assessment-9/solution.js
+++ assessment/assessment-9/solution.js
@@ -1,1 +1,10 @@
-console.log("Bakhtiyor");
+function generateAlmostDivisibleBy13(num) {
+    var sum = 0;
+    var array = [];
+    for (var i = 1; i < num; i++) {
+        if ((i + 1) % 13 === 0 || (i - 1) % 13 === 0) {
+            sum++;
+        }
+    }
+    return array.push(sum);
+}
